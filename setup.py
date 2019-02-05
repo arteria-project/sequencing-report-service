c64ae9646a13057107dee6c7212200f33d62d18f
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2019-02-05 09:29:15 +0100

    Ensure all items under sequencing_report_service are installed, but not tests

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     include_package_data=True,
     keywords='sequencing_report_service',
     name='sequencing_report_service',
-    packages=find_packages(),
+    packages=find_packages(include=["sequencing_report_service*"]),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
